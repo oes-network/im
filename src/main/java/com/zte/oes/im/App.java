d675faa0e3ee2e79d358def4ee5e0960f1e4a70d
fanfan <yu.qifan@zte.com.cn>
2017-04-17 19:58:07 +0800

    create maven project

---
+++ src/main/java/com/zte/oes/im/App.java
@@ -0,0 +1,13 @@
+package com.zte.oes.im;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
